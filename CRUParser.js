5e212bc7ef9c545800a0672a906b8be5a96d74c6
SwanBones <xuan.boone@gmail.com>
2023-11-24 13:17:12 +0100

    ajout de commentaires pour aider à l'implémentation du parseur

--- CRUParser.js
+++ CRUParser.js
@@ -5,8 +5,8 @@ var CRU = require('./CRU');
 var CRUParser = function(sTokenize, sParsedSymb){
 	// The list of POI parsed from the input file.
 	this.parsedCRU = [];
-	this.symb = ["+","//"];
-	this.showTokenize = sTokenize;
+	this.symb = ["+","","//"];
+	this.showTokenize = sTokenize;//booléen initié à l'instantiation. vrai = console.log(liste de ce qu'on a parsé, c.f. ligne 30 ). faux = affiche rien
 	this.showParsedSymbols = sParsedSymb;
 	this.errorCount = 0;
 }
@@ -16,26 +16,27 @@ var CRUParser = function(sTokenize, sParsedSymb){
 
 // tokenize : tranform the data input into a list
 // <eol> = CRLF
-CRUParser.prototype.tokenize = function(data){
-	var separator = /(\r\n|: )/;
-	data = data.split(separator);
-	data = data.filter((val, idx) => !val.match(separator)); 					
-	return data;
+CRUParser.prototype.tokenize = function(data){//data est un string de tout ce qui se trouve dans le fichier
+	var separator = /(\r\n|=|, )/;//le separateur est soit "\r\n" (retour à la ligne de windows), soit "=", soit ","
+	data = data.split(separator);//on prend le string, on le transforme en liste, avec comme séparateur ce qu'on a défini auparavant
+	data = data.filter((val, idx) => !val.match(separator));//on retire les séparateurs de la liste 					
+	console.log(data)
+	return data;//on retourne la liste de strings, sans les séparateurs.
 }
 
 // parse : analyze data by calling the first non terminal rule of the grammar
 CRUParser.prototype.parse = function(data){
-	var tData = this.tokenize(data);
-	if(this.showTokenize){
+	var tData = this.tokenize(data);//on appelle tokenize pour transformer le texte en liste
+	if(this.showTokenize){//
 		console.log(tData);
 	}
-	this.listPoi(tData);
+	this.listPoi(tData);//on commence à analyser la liste. c.f. ligne 88
 }
 
 // Parser operand
 
 CRUParser.prototype.errMsg = function(msg, input){
-	this.errorCount++;
+	this.errorCount++;//si error count est 0, le fichier est conforme.
 	console.log("Parsing Error ! on "+input+" -- msg : "+msg);
 }
 
@@ -63,8 +64,14 @@ CRUParser.prototype.accept = function(s){
 
 
 // check : check whether the arg elt is on the head of the list
+//entrées: 				s = symbole à vérifier
+//						input = data
+
 CRUParser.prototype.check = function(s, input){
-	if(this.accept(input[0]) == this.accept(s)){
+
+//accept retourne: 	un int (index). = le premier symbole de input fait partie de la liste de symboles qu'on a défini auparavant. l'index est sa position dans la liste des symboles
+//					un bool (faux)  = pas reconnu dans la liste de symbole
+	if(this.accept(input[0]) == this.accept(s)){//si le mot à analyser est acceptable, et s (symbole entré) aussi. (ou non acceptable et s aussi)
 		return true;	
 	}
 	return false;
@@ -73,6 +80,8 @@ CRUParser.prototype.check = function(s, input){
 // expect : expect the next symbol to be s.
 CRUParser.prototype.expect = function(s, input){
 	if(s == this.next(input)){
+		//next retourne le prochain symbole.
+		//next utilise "input.shift() sur la liste. important car shift élimine l'élément en tête de la liste."
 		//console.log("Reckognized! "+s)
 		return true;
 	}else{
@@ -85,21 +94,23 @@ CRUParser.prototype.expect = function(s, input){
 // Parser rules
 // <liste_poi> = *(<poi>) "$$"
 CRUParser.prototype.listPoi = function(input){
-	this.poi(input);
-	this.expect("$$", input);
+	this.poi(input);//analyser le corps du texte (<poi>)
+	this.expect("$$", input);//lance une erreur si le symbole est pas recconu. "input" est la data / la liste
 }
 
 // <poi> = "START_POI" <eol> <body> "END_POI"
+// <cru> = "+SEANCE" <eol> <body> "END_POI"
 CRUParser.prototype.poi = function(input){
 
 	if(this.check("START_POI", input)){
-		this.expect("START_POI", input);
-		var args = this.body(input);
-		var p = new POI(args.nm, args.lt, args.lg, []);
+		this.expect("START_POI", input);//skip un start POI, lancer une erreur si le start poi n'est pas trouvé
+		var args = this.body(input);//<body> = <name> <eol> <latlng> <eol> <optional>
+		//puis créer un nouveau POI à partir des données qu'on vient de parser avec this.body(input)
+		var p = new POI(args.sc, args.typ, args.pl, args.j, args.hd, args.hf, args.pl, args.sG, args.salle);//sc, typ, pl, j, hd, hf, sG, salle
 		this.note(input, p);
 		this.expect("END_POI",input);
-		this.parsedPOI.push(p);
-		if(input.length > 0){
+		this.parsedPOI.push(p);//ajouter dans la liste de POI parsé, l'objet qu'on vient de créer
+		if(input.length > 0){//continuer à parser si la liste est tojours pas vide
 			this.poi(input);
 		}
 		return true;
